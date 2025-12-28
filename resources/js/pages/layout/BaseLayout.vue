63cd86c54f31b29e79afaba69eee7412a77023f6
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 15:13:00 -0300

    feat: creating the base layout of all the pages

---
+++ resources/js/pages/layout/BaseLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <slot name="header"></slot>
+    <slot name="content"></slot>
+</template>
+
+<style>
+    body {
+        width: 100vw;
+        height: 100vh;
+        background-color: #191919;
+        color: white;
+    }
+
+    div#app {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
