7ca21708ebaec47bfb0c495289b77ca534e7b158
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 18:01:10 -0300

    feat: exposing the browser macros

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,4 +1,7 @@
 <?php
 
 return [
+    App\Providers\BrowserCookieRegistrationProvider::class,
+    App\Providers\BrowserAssertPathIsDecodedURLProvider::class,
+    App\Providers\BrowserWaitForLocationDecodedProvider::class
 ];
