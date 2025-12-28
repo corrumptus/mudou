b874d3d372778e3d2b31b5aa3bc98230b1e3caf1
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 19:17:21 -0300

    test: creating the show course page object

---
+++ tests/Browser/Pages/Admin/ShowCoursePage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Browser\Pages\Admin;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\Page;
+
+class ShowCoursePage extends Page
+{
+    private string $courseName;
+
+    public function __construct(Browser $browser, string $token, string $courseName) {
+        $this->browser = $browser;
+        $this->courseName = $courseName;
+
+        $browser
+            ->loginCookie($token)
+            ->visit($this->url())
+            ->waitFor($this->getLastRenderElement());
+
+        $this->assert($browser);
+    }
+
+    /**
+     * Get the URL for the page.
+     */
+    public function url(): string
+    {
+        return "/course/$this->courseName";
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     */
+    public function assert(Browser $browser): void
+    {
+        $browser
+            ->assertPathIsDecoded($this->url())
+            ->assertPresent('@edit')
+            ->assertPresent('@infos');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array<string, string>
+     */
+    public function elements(): array
+    {
+        return [
+            '@edit' => '[dusk="edit"]',
+            '@infos' => '[dusk="infos"]'
+        ];
+    }
+
+    public function getLastRenderElement(): string
+    {
+        return '@infos';
+    }
+
+    public function clickEdit() {
+        $this->browser->click('@edit');
+
+        $this->browser->waitForLocationDecoded("/course/$this->courseName/edit");
+    }
+}
