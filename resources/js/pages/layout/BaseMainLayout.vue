b56fb536f1e13a7e044ff06373ef491d9dafde41
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 15:13:12 -0300

    feat: creating the base main layout of all the pages

---
+++ resources/js/pages/layout/BaseMainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+        <slot name="navbar"></slot>
+        <section>
+            <slot name="content"></slot>
+        </section>
+        <slot name="sidebar"></slot>
+    </main>
+</template>
+
+<style scoped>
+    main {
+        width: 100%;
+        height: 100%;
+        padding: .5em;
+        display: flex;
+        gap: 10px;
+        overflow: hidden;
+    }
+    
+    section {
+        width: 100%;
+        height: 100%;
+        border-radius: 10px;
+        background-color: #fdfdfd;
+        color: black;
+        overflow: hidden;
+    }
+</style>
