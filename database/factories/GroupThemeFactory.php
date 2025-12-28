808e36a814163eff4ae8ad9bfca4b84f05956e84
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 15:11:35 -0300

    feat: creating the group theme factory

---
+++ database/factories/GroupThemeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Group;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\GroupTheme>
+ */
+class GroupThemeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "id" => rand(),
+            "theme" => fake()->name(),
+            "group_id" => Group::factory()
+        ];
+    }
+}
