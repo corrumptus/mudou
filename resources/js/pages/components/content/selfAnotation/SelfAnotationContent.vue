ee2df42fe085b9c4cacd4e410138a0c394a0dcd2
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 17:08:53 -0300

    feat: creating the prototype of the self anotation page

---
+++ resources/js/pages/components/content/selfAnotation/SelfAnotationContent.vue
@@ -0,0 +1,54 @@
+<template>
+    <ContentHeader>
+        <button>
+            <img src="https://cdn-icons-png.flaticon.com/512/764/764599.png" alt="pencil">
+        </button>
+    </ContentHeader>
+    <div class="selfAnotation">
+        <div class="anotation">
+            {{ anotation.text }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import ContentHeader from '@header/ContentHeader.vue';
+
+    defineProps({
+        anotation: {
+            type: Object,
+            required: true
+        }
+    });
+</script>
+
+<style scoped>
+    button {
+        width: 40px;
+        height: 40px;
+        border-radius: 100%;
+        background-color: #b4b4b4;
+        cursor: pointer;
+    }
+
+    img {
+        width: 26px;
+        height: 26px;
+        margin: auto;
+        transform: translate(1px);
+    }
+
+    .selfAnotation {
+        height: 100%;
+        padding: 1em;
+        overflow: hidden;
+    }
+
+    .anotation {
+        height: 100%;
+        padding: .3em .6em;
+        border-radius: 1em;
+        box-shadow: inset 0px 0px 4px black;
+        overflow: auto;
+    }
+</style>
---
+++ resources/js/pages/routes/SelfAnotation.vue
@@ -0,0 +1,50 @@
+<template>
+    <BaseLayout>
+        <template #header>
+            <HomeHeader :pageName="`${classroom.name} - Minha Anotação`" />
+        </template>
+
+        <template #content>
+            <BaseMainLayout>
+                <template #navbar>
+                    <ClassroomNavBar
+                        :classroom="classroom"
+                        :classes="classes"
+                        :selectedClassElement="{ classId: '', elementId: '' }"
+                        :collapsedClasses="collapsedClasses"
+                        :switchCollapse="switchCollapse"
+                    />
+                </template>
+
+                <template #content>
+                    <SelfAnotationContent
+                        :anotation="selfAnotation"
+                    />
+                </template>
+            </BaseMainLayout>
+        </template>
+    </BaseLayout>
+</template>
+
+<script setup>
+    import { usePage } from '@inertiajs/vue3';
+    import BaseLayout from '@layout/BaseLayout.vue';
+    import BaseMainLayout from '@layout/BaseMainLayout.vue';
+    import HomeHeader from '@header/HomeHeader.vue';
+    import ClassroomNavBar from '@navbar/class/ClassroomNavBar.vue';
+    import SelfAnotationContent from '@content/selfAnotation/SelfAnotationContent.vue';
+    import useCollapsedClasses from '@hooks/collapsedClasses';
+
+    const page = usePage();
+
+    const { classroom, classes, selfAnotation } = page.props;
+
+    const [ collapsedClasses, switchCollapse ] = useCollapsedClasses(classes);
+</script>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
