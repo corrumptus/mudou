9a57750e58aad8f2f9052c1d9cb7cc69a6c3eb7f
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 19:16:40 -0300

    test: creating the edit course page object

---
+++ tests/Browser/Pages/Admin/EditCoursePage.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Browser\Pages\Admin;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\Page;
+
+class EditCoursePage extends Page
+{
+    private string $courseName;
+
+    public function __construct(Browser $browser, string $token, string $courseName) {
+        $this->browser = $browser;
+        $this->courseName = $courseName;
+
+        $browser
+            ->loginCookie($token)
+            ->visit($this->url())
+            ->waitFor($this->getLastRenderElement());
+
+        $this->assert($browser);
+    }
+
+    /**
+     * Get the URL for the page.
+     */
+    public function url(): string
+    {
+        return "/course/$this->courseName/edit";
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     */
+    public function assert(Browser $browser): void
+    {
+        $browser
+            ->assertPathIsDecoded($this->url())
+            ->assertPresent('@name')
+            ->assertPresent('@code')
+            ->assertPresent('@amountSemesters')
+            ->assertPresent('@confirm')
+            ->assertPresent('@cancel');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array<string, string>
+     */
+    public function elements(): array
+    {
+        return [
+            '@name' => '[dusk="name"]',
+            '@code' => '[dusk="code"]',
+            '@amountSemesters' => '[dusk="amountSemesters"]',
+            '@confirm' => '[dusk="confirm"]',
+            '@cancel' => '[dusk="cancel"]',
+        ];
+    }
+
+    public function getLastRenderElement(): string
+    {
+        return '@cancel';
+    }
+
+    public function fillAndSubmit(array $user) {
+        $this->browser->type('@name', $user['name']);
+        $this->browser->type('@code', $user['code']);
+        $this->browser->type('@amountSemesters', $user['amount_semesters']);
+
+        $this->browser->click('@confirm');
+
+        sleep(2); //wait the async function to be handled
+    }
+}
