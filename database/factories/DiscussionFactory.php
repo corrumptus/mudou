050146423d21a9dab56c0da57b6f980458111476
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 15:10:46 -0300

    feat: creating the discussion factory

---
+++ database/factories/DiscussionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Classroom;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Discussion>
+ */
+class DiscussionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "id" => rand(),
+            "title" => fake()->text(),
+            "description" => fake()->text(),
+            "created_at" => fake()->dateTime(),
+            "classroom_id" => Classroom::factory(),
+            "user_id" => User::factory()
+        ];
+    }
+}
