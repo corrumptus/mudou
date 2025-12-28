d7cfdd3820113b2cffa02fa4193115fea73bb803
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 17:09:39 -0300

    feat: creating the prototype of the other anotation page

---
+++ resources/js/pages/components/content/otherAnotation/OtherAnotationContent.vue
@@ -0,0 +1,35 @@
+<template>
+    <ContentHeader />
+    <div class="otherAnotation">
+        <div class="anotation">
+            {{ text }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import ContentHeader from '@header/ContentHeader.vue';
+
+    defineProps({
+        text: {
+            type: String,
+            required: true
+        }
+    });
+</script>
+
+<style scoped>
+    .otherAnotation {
+        height: 100%;
+        padding: 1em;
+        overflow: hidden;
+    }
+
+    .anotation {
+        height: 100%;
+        padding: .3em .6em;
+        border-radius: 1em;
+        box-shadow: inset 0px 0px 4px black;
+        overflow: auto;
+    }
+</style>
---
+++ resources/js/pages/routes/OtherAnotation.vue
@@ -0,0 +1,50 @@
+<template>
+    <BaseLayout>
+        <template #header>
+            <HomeHeader :pageName="`${classroom.name} - Anotação de ${otherAnotation.name}`" />
+        </template>
+
+        <template #content>
+            <BaseMainLayout>
+                <template #navbar>
+                    <ClassroomNavBar
+                        :classroom="classroom"
+                        :classes="classes"
+                        :selectedClassElement="{ classId: '', elementId: '' }"
+                        :collapsedClasses="collapsedClasses"
+                        :switchCollapse="switchCollapse"
+                    />
+                </template>
+
+                <template #content>
+                    <OtherAnotationContent
+                        :text="otherAnotation.text"
+                    />
+                </template>
+            </BaseMainLayout>
+        </template>
+    </BaseLayout>
+</template>
+
+<script setup>
+    import { usePage } from '@inertiajs/vue3';
+    import BaseLayout from '@layout/BaseLayout.vue';
+    import BaseMainLayout from '@layout/BaseMainLayout.vue';
+    import HomeHeader from '@header/HomeHeader.vue';
+    import ClassroomNavBar from '@navbar/class/ClassroomNavBar.vue';
+    import OtherAnotationContent from '@content/otherAnotation/OtherAnotationContent.vue';
+    import useCollapsedClasses from '@hooks/collapsedClasses';
+
+    const page = usePage();
+
+    const { classroom, classes, otherAnotation } = page.props;
+
+    const [ collapsedClasses, switchCollapse ] = useCollapsedClasses(classes);
+</script>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
