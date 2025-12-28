c03882f1e13478e9000644ff37f8cd1e200248f8
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 17:48:14 -0300

    build: creating the ts paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -24,4 +25,19 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: [
+            { find: "@pages", replacement: path.resolve(__dirname, "./resources/js/pages/routes") },
+            { find: "@layout", replacement: path.resolve(__dirname, "./resources/js/pages/layout") },
+            { find: "@header", replacement: path.resolve(__dirname, "./resources/js/pages/components/header") },
+            { find: "@navbar", replacement: path.resolve(__dirname, "./resources/js/pages/components/navbar") },
+            { find: "@content", replacement: path.resolve(__dirname, "./resources/js/pages/components/content") },
+            { find: "@sidebar", replacement: path.resolve(__dirname, "./resources/js/pages/components/sidebar") },
+            { find: "@hooks", replacement: path.resolve(__dirname, "./resources/js/hooks") },
+            { find: "@data", replacement: path.resolve(__dirname, "./resources/js/data") },
+            { find: "@request", replacement: path.resolve(__dirname, "./resources/js/request") },
+            { find: "@utils", replacement: path.resolve(__dirname, "./resources/js/utils") },
+            { find: "@type", replacement: path.resolve(__dirname, "./resources/js/types") }
+        ]
+    }
 });
