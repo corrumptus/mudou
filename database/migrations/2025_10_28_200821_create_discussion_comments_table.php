8bdf61f3b117766c980941716099cc3ff5a88ecf
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 01:44:37 -0300

    feat: creating the discussion comments table

---
+++ database/migrations/2025_10_28_200821_create_discussion_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Discussion;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discussion_comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment', 4096);
+            $table->timestamp('created_at');
+            $table->foreignIdFor(Discussion::class);
+            $table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discussion_comments');
+    }
+};
