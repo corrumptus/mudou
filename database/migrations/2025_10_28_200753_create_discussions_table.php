ced5c0b872f5715b403c89956f31972768a631bd
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 01:43:14 -0300

    feat: creating the discussions table

---
+++ database/migrations/2025_10_28_200753_create_discussions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Classroom;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discussions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description', 4096);
+            $table->timestamp('created_at');
+            $table->foreignIdFor(Classroom::class);
+            $table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discussions');
+    }
+};
