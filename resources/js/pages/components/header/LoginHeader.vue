d6a849d30a8aa4f10b5cac72bd84efdfb0ab645d
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 15:36:04 -0300

    feat: creating a login header

---
+++ resources/js/pages/components/header/LoginHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <span>Mudou</span>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        width: 100%;
+        padding: .5em 1em;
+    }
+
+    span {
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+</style>
