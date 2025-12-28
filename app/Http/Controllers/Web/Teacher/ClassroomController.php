acb741fc504498cbd4ba143284615f45957acca5
corrumptus <lazarinilucas42@gmail.com>
2025-12-28 14:23:13 -0300

    feat: creating the teachers classroom web controller

---
+++ app/Http/Controllers/Web/Teacher/ClassroomController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Web\Teacher;
+
+use App\Data\Classroom\Classroom as ClassroomDTO;
+use App\Models\Course;
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ClassroomController
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $courseName = $request->route('courseName');
+
+        $subjectName = $request->route('subjectName');
+
+        $classroomId = $request->route('classroomId');
+
+        $user = Auth::user();
+
+        return Inertia::render(
+            'routes/Teacher/Classroom',
+            [
+                ...$this->pageProps($courseName, $subjectName, $classroomId, $user),
+                "type" => "teacher"
+            ]
+        );
+    }
+
+    public static function pageProps(string $courseName, string $subjectName, int $classroomId, User $user) {
+        $classroom = ClassroomController::query($courseName, $subjectName, $classroomId, $user);
+
+        return $classroom == null ?
+            ClassroomDTO::fromNullPath($courseName, $subjectName, $classroomId)
+            :
+            ClassroomDTO::fromClassroomModelIncomplete($classroom);
+    }
+
+    private static function query(string $courseName, string $subjectName, int $classroomId, User $user) {
+        $classroom = Course::firstWhere('name', $courseName)
+            ?->subjects()
+            ->firstWhere('name', $subjectName)
+            ?->classrooms()
+            ->find($classroomId);
+
+        if ($classroom == null)
+            return null;
+
+        if (!$classroom->teachers()->where('id', $user->id)->exists())
+            return null;
+
+        return $classroom;
+    }
+}
